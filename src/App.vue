4709dd321e7e5692c2a4f29f7f861350e0b46884
Neil Balch <neilbalch@gmail.com>
2019-03-30 13:41:07 -0700

    Hrm....

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <h1>This is a more senseful text.</h1>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
