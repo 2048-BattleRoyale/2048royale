718fca61cd2a06aa2bddcb9ef4da0367ce0799e8
Neil Balch <neilbalch@gmail.com>
2019-07-06 19:58:12 -0700

    Ooh, more prototyping...

--- Server/index.js
+++ Server/index.js
@@ -1,4 +1,8 @@
 var http = require('http');
+var url = require('url');
+var queryString = require('querystring');
+
+// https://ilovecoding.org/courses/nodejs/lessons/create-a-simple-http-server-with-nodejs
 
 http.createServer(function (request, response) {
   response.writeHead(200, {
@@ -7,4 +11,9 @@ http.createServer(function (request, response) {
   });
   response.write('Hello Node JS Server Response');
   response.end();
+
+  pathName = url.parse(request.url).pathname;
+  query = url.parse(request.url).query;
+  console.log('pathName: ' + pathName);
+  console.log('query: ' + query);
 }).listen(7000);
