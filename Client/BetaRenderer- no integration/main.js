c791a8fb0adc9185c1b4aa707750884ba09c6a6c
Aidan Smith <lolbot55555@gmail.com>
2019-07-09 16:47:58 -0700

    Integrated an anime JS demo, for future reference.

---
+++ Client/BetaRenderer- no integration/main.js
@@ -0,0 +1,25 @@
+
+
+anime({
+  targets: 'div.box',
+  translateY: [
+    { value: 200, duration: 500 },
+    { value: 0, duration: 500 },
+  ],
+  rotate:{
+    value: '.75turn',
+    easing: 'easeInOutSine'
+  },
+  backgroundColor:[{
+      value:'#FFF',
+      easing: 'spring',
+      duration:500
+    },
+  {
+    value:'#9b00ff',
+  }],
+  delay: function(el, i, l){ return i * 500},
+  loop:true,
+  drection:'alternate'
+  
+});
