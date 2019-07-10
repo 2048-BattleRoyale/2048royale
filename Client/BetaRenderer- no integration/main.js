112d4f023fcb6ce8abc2afb50631b36c527d2eb9
Aidan Smith <lolbot55555@gmail.com>
2019-07-09 20:57:30 -0700

    DIV animation test- Array hellscape

--- Client/BetaRenderer- no integration/main.js
+++ Client/BetaRenderer- no integration/main.js
@@ -16,17 +16,30 @@ for (index = 1; index < 14; index++) {
   gridContainer.appendChild(horizontal_line);  
   document.getElementById("hline"+index.toString()).style.transform = "translate("+(0).toString()+"vmin,"+(5.8*index).toString()+"vmin)";
 }
-//Variable Declarations
+//Variable&Class Declarations
 
 var userToken = "garbage placeholder";
 var score = 0;
-var packetSent = false;
+var animationDuration=500;
 var lastStrokeTime;
+var packetSent = false;
 var playerAlive = true;
 var currentArray=[];
 var modernArray=[];
 var currentTiles=[];
 
+class Tile {
+  constructor(x,y,value) {
+    this.x=x;
+    this.y=y;
+    this.value=value;
+  }
+
+  deleteSelf() {
+    alert("Hewwo, incompwete feature");
+  }
+
+}
 //Primary Functions
 function removeTile(removeid) {
 
@@ -43,12 +56,15 @@ function drawAtCoords(X,Y,Value) {
 function firstDraw() {
 
 }
+function deleteTile() {
+
+}
 function drawMovement() {
 
 }
 //Listeners
 document.addEventListener('keydown', function(event){
-  //alert(event.keyCode); (Uncomment this line if you need to add future keyswitch codes
+  //alert(event.keyCode); (Uncomment this line if you need to add future keyswitch codes)
   switch(event.keyCode) {
     case 87:
     case 38:
@@ -83,28 +99,28 @@ while (false) {
 
 
 
-  /*
+  
 anime({
-  targets: 'div.box',
+  targets: 'div.grid',
   translateY: [
     { value: 200, duration: 500 },
     { value: 0, duration: 500 },
+    { value: 200, duration: 500 },
   ],
   rotate:{
     value: '.75turn',
     easing: 'easeInOutSine'
   },
   backgroundColor:[{
-      value:'#FFF',
+      value:'#2b01ff',
       easing: 'spring',
       duration:500
     },
   {
-    value:'#9b00ff',
+    value:'#9b00ff', duration:500
   }],
-  delay: function(el, i, l){ return i * 500},
+  delay:500,
   loop:true,
-  drection:'alternate'
+  direction:'alternate'
   
 });
-*/
