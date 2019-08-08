3012c4e4938453e56204464833e37bea4fb4cd8f
Aidan Smith <lolbot55555@gmail.com>
2019-08-08 14:58:49 -0700

    Color SHIFTING Update

---
+++ Client/js/cookietable.js
@@ -0,0 +1,28 @@
+
+
+
+var testTheme={ 
+  "2":"A8DEF5",
+  "4":"AFDBF3",
+  "8":"B7D8F2",
+  "16":"BFD5F1",
+  "32":"C7D2EF",
+  "64":"CFCFEE",
+  "128":"D6CCED",
+  "256":"DEC9EC",
+  "512":"E6C6EA",
+  "1024":"EEC3E9",
+  "2048":"F6C0E8",
+  "4096":"FEBEE7",
+  "8192":"FF8AD5",
+  "16384":"FF47BD"
+  }
+
+function changeJSON(number, color) {
+  //console.log(color);
+  testTheme[number.toString()]=color;
+}
+$('.exportJSON').on('click', function(event) {
+  $.cookie("colorTheme", JSON.stringify(testTheme));
+
+});
