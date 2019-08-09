b72974cfd0a61fdeba70900ed926529fd7272fef
Neil Balch <neilbalch@gmail.com>
2019-08-08 17:26:01 -0700

    Better UI on wsMultiDemoClient

--- Server/wsMultiDemoClient/app.js
+++ Server/wsMultiDemoClient/app.js
@@ -5,6 +5,43 @@ window.onload = function () {
   handleBox(4);
 }
 
+loadSignupMsgsBtn = document.getElementById("loadSignupMsgs");
+loadSignupMsgsBtn.onclick = function (event) {
+  var box1 = document.getElementById('message' + 1);
+  var box2 = document.getElementById('message' + 2);
+  var box3 = document.getElementById('message' + 3);
+  var box4 = document.getElementById('message' + 4);
+
+  box1.value = JSON.stringify({
+    msgType: "signup",
+    sessionID: "bazb7aMs",
+    name: "Billy Bob"
+  });
+  box2.value = JSON.stringify({
+    msgType: "signup",
+    sessionID: "bazb7afs",
+    name: "Jane Bob"
+  });
+  box3.value = JSON.stringify({
+    msgType: "signup",
+    sessionID: "bazb3aMs",
+    name: "Joe Smith"
+  });
+  box4.value = JSON.stringify({
+    msgType: "signup",
+    sessionID: "bwrb3aMs",
+    name: "Jack Sprat"
+  });
+};
+
+sendAllMsgsBtn = document.getElementById("sendAllMsgs");
+sendAllMsgsBtn.onclick = function (event) {
+  document.getElementById("submit" + 1).click();
+  document.getElementById("submit" + 2).click();
+  document.getElementById("submit" + 3).click();
+  document.getElementById("submit" + 4).click();
+}
+
 function handleBox(number) {
   // Get references to elements on the page.
   var form = document.getElementById('message-form' + number);
@@ -13,24 +50,20 @@ function handleBox(number) {
   var socketStatus = document.getElementById('status' + number);
   var closeBtn = document.getElementById('close' + number);
 
-
   // Create a new WebSocket.
   // var socket = new WebSocket('ws://echo.websocket.org');
   var socket = new WebSocket('ws://localhost:8000');
-
 
   // Handle any errors that occur.
   socket.onerror = function (error) {
     console.log('WebSocket Error: ' + error);
   };
 
-
   // Show a connected message when the WebSocket is opened.
   socket.onopen = function (event) {
     socketStatus.innerHTML = 'Connected to: ' + event.currentTarget.URL;
     socketStatus.className = 'open';
   };
-
 
   // Handle messages sent by the server.
   socket.onmessage = function (event) {
@@ -39,13 +72,11 @@ function handleBox(number) {
       message + '</li>';
   };
 
-
   // Show a disconnected message when the WebSocket is closed.
   socket.onclose = function (event) {
     socketStatus.innerHTML = 'Disconnected from WebSocket.';
     socketStatus.className = 'closed';
   };
-
 
   // Send a message when the form is submitted.
   form.onsubmit = function (e) {
@@ -53,20 +84,16 @@ function handleBox(number) {
 
     // Retrieve the message from the textarea.
     var message = messageField.value;
-
     // Send the message through the WebSocket.
     socket.send(message);
-
     // Add the message to the messages list.
     messagesList.innerHTML += '<li class="sent"><span>Sent:</span>' + message +
       '</li>';
 
     // Clear out the message field.
     messageField.value = '';
-
     return false;
   };
-
 
   // Close the WebSocket connection when the close button is clicked.
   closeBtn.onclick = function (e) {
@@ -74,7 +101,6 @@ function handleBox(number) {
 
     // Close the WebSocket.
     socket.close();
-
     return false;
   };
 
