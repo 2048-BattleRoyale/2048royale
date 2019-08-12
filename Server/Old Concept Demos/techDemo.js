ee96c48485f800d2cdca7058e24f8f968dfd75f5
Neil Balch <neilbalch@gmail.com>
2019-08-12 14:16:35 -0700

    Improve tech demo

--- Server/Old Concept Demos/techDemo.js
+++ Server/Old Concept Demos/techDemo.js
@@ -7,15 +7,20 @@ console.log("Initial Setup:");
 board.initBoard();
 board.logTileInfo();
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+function getInputAndAct() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
 
-rl.question('Move direction? ', (answer) => {
-  console.log(answer);
-  board.handleBoardMove(answer, 1);
-  board.logTileInfo();
+  rl.question('Move direction? ', (answer) => {
+    console.log(answer);
+    board.handleBoardMove(answer, 1);
+    board.logTileInfo();
 
-  rl.close();
-});
+    rl.close();
+    getInputAndAct();
+  });
+}
+
+getInputAndAct();
--- Server/wsMultiDemoClient/app.js
+++ Server/wsMultiDemoClient/app.js
@@ -1,3 +1,66 @@
+function handleBox(number) {
+  // Get references to elements on the page.
+  var form = document.getElementById('message-form' + number);
+  var msgField = document.getElementById('message' + number);
+  var msgHistory = document.getElementById('messages' + number);
+  var status = document.getElementById('status' + number);
+  var closeBtn = document.getElementById('close' + number);
+
+  // Create a new WebSocket.
+  // var socket = new WebSocket('ws://echo.websocket.org');
+  var sck = new WebSocket('ws://localhost:8000');
+
+  // Handle any errors that occur.
+  sck.onerror = function (error) {
+    console.log('WebSocket Error: ' + error);
+  };
+
+  // Show a connected message when the WebSocket is opened.
+  sck.onopen = function (event) {
+    status.innerHTML = 'Connected to: ' + event.currentTarget.URL;
+    status.className = 'open';
+  };
+
+  // Handle messages sent by the server.
+  sck.onmessage = function (event) {
+    var message = event.data;
+    msgHistory.innerHTML += '<li class="received"><span>Received:</span>' +
+      message + '</li>';
+  };
+
+  // Show a disconnected message when the WebSocket is closed.
+  sck.onclose = function (event) {
+    status.innerHTML = 'Disconnected from WebSocket.';
+    status.className = 'closed';
+  };
+
+  // Send a message when the form is submitted.
+  form.onsubmit = function (e) {
+    e.preventDefault();
+
+    // Retrieve the message from the textarea.
+    var message = msgField.value;
+    // Send the message through the WebSocket.
+    sck.send(message);
+    // Add the message to the messages list.
+    msgHistory.innerHTML += '<li class="sent"><span>Sent:</span>' + message +
+      '</li>';
+
+    // Clear out the message field.
+    msgField.value = '';
+    return false;
+  };
+
+  // Close the WebSocket connection when the close button is clicked.
+  closeBtn.onclick = function (e) {
+    e.preventDefault();
+
+    // Close the WebSocket.
+    sck.close();
+    return false;
+  };
+};
+
 window.onload = function () {
   handleBox(1);
   handleBox(2);
@@ -41,67 +104,3 @@ sendAllMsgsBtn.onclick = function (event) {
   document.getElementById("submit" + 3).click();
   document.getElementById("submit" + 4).click();
 }
-
-function handleBox(number) {
-  // Get references to elements on the page.
-  var form = document.getElementById('message-form' + number);
-  var messageField = document.getElementById('message' + number);
-  var messagesList = document.getElementById('messages' + number);
-  var socketStatus = document.getElementById('status' + number);
-  var closeBtn = document.getElementById('close' + number);
-
-  // Create a new WebSocket.
-  // var socket = new WebSocket('ws://echo.websocket.org');
-  var socket = new WebSocket('ws://localhost:8000');
-
-  // Handle any errors that occur.
-  socket.onerror = function (error) {
-    console.log('WebSocket Error: ' + error);
-  };
-
-  // Show a connected message when the WebSocket is opened.
-  socket.onopen = function (event) {
-    socketStatus.innerHTML = 'Connected to: ' + event.currentTarget.URL;
-    socketStatus.className = 'open';
-  };
-
-  // Handle messages sent by the server.
-  socket.onmessage = function (event) {
-    var message = event.data;
-    messagesList.innerHTML += '<li class="received"><span>Received:</span>' +
-      message + '</li>';
-  };
-
-  // Show a disconnected message when the WebSocket is closed.
-  socket.onclose = function (event) {
-    socketStatus.innerHTML = 'Disconnected from WebSocket.';
-    socketStatus.className = 'closed';
-  };
-
-  // Send a message when the form is submitted.
-  form.onsubmit = function (e) {
-    e.preventDefault();
-
-    // Retrieve the message from the textarea.
-    var message = messageField.value;
-    // Send the message through the WebSocket.
-    socket.send(message);
-    // Add the message to the messages list.
-    messagesList.innerHTML += '<li class="sent"><span>Sent:</span>' + message +
-      '</li>';
-
-    // Clear out the message field.
-    messageField.value = '';
-    return false;
-  };
-
-  // Close the WebSocket connection when the close button is clicked.
-  closeBtn.onclick = function (e) {
-    e.preventDefault();
-
-    // Close the WebSocket.
-    socket.close();
-    return false;
-  };
-
-};
