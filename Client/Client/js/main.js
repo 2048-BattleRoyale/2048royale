352a2436d4b2b1ebc58d96b58384eb44efe5fc4a
Aidan Smith <lolbot55555@gmail.com>
2019-08-18 11:33:43 -0700

    Fixed Renderer!

--- Client/Client/js/main.js
+++ Client/Client/js/main.js
@@ -26,8 +26,6 @@ var animationDuration=500;
 var lastStrokeTime;
 var packetSent = false;
 var playerAlive = true;
-var currentArray=[];
-var modernArray=[];
 var players=[];
 var eventuallyRemove=[];
 var board = {"players":{1:{"Name":"String","Score":0},2:{"Name":"String","Score":0},3:{"Name":"String","Score":4},4:{"Name":"String","Score":0}},"boxes":{"1":{"enabled":true,"tileNum":2,"tileId":1,"owner":1,"justMerged":false},"2":{"enabled":true,"tileNum":512,"tileId":85,"owner":2,"justMerged":false},"3":{"enabled":true,"tileNum":4096,"tileId":96,"owner":1,"justMerged":false},"4":{"enabled":true,"tileNum":128,"tileId":37,"owner":1,"justMerged":false},"5":{"enabled":true,"tileNum":32,"tileId":193,"owner":1,"justMerged":false},"6":{"enabled":true,"tileNum":2,"tileId":112,"owner":1,"justMerged":false},"7":{"enabled":true,"tileNum":256,"tileId":196,"owner":1,"justMerged":false}}};
@@ -43,8 +41,12 @@ var lockedBoxes=[];
 var lightColor='F9F6F2';
 var darkColor='776E65';
 var transformnumx=5.741924954411;
+var oldnew;
+var oldBoard={"players":{},"boxes":{}};
 var transformnumy=5.747124954411;
 var debug=false;
+var recentboard={"players":{},"boxes":{}};
+var okayWork=true;
 //Test/Example board used for testing out a real board object.
 //Color Profiles Stored Dynamically Online- this is the default
 var theme1={ //This is the standard 2048 theme
@@ -88,8 +90,10 @@ class Tile {
 //Practical side-functions
 function findCurrentAnim(id,xory) { // Useful in the console to find X or Y
   var transforms=document.getElementById('tile'+id.toString()).style.transform;
-  transformY=((transforms.split('translateY'))[1].slice(1,(transforms.split('translateY'))[1].length-5));
-  transformX=((transforms.split('translateY'))[1].split('translateX'))[0].slice(1,(transforms.split('translateY'))[1].split('translateX')[0].length-5);
+  transformY=((transforms.split('translateY'))[1].slice(0,(transforms.split('translateY'))[1].length-5));
+  transformY=transformY.slice(1,transformY.length-5)
+  transformX=((transforms.split('translateY'))[1].split('translateX'))[0].slice(0,(transforms.split('translateY'))[1].split('translateX')[0].length-5);
+  transformX=transformX.slice(1,transformX.length-5)
   if (xory='X') {
     return transformX;
   }
@@ -100,6 +104,7 @@ function findCurrentAnim(id,xory) { // Useful in the console to find X or Y
     console.log("Fatal error on line 85.")
   }
 }
+
 
 function calcX(tileId) { //Used to parse the modulo values given in the Tile creation of newTile
   if(tileId==1) {
@@ -120,14 +125,7 @@ function calcY(tileId) { //See above... but for Y
     return Math.floor(tileId/14+1);
   }
 }
-function findposbyID(ID) { //Give this function an ID from a current Tile and it'll tell you its position in the array
-  for (i=0;i<currentArray.length;i++) {
-      if (currentArray[i].id == ID) {
-        return i;
-        break;
-      }
-  }
-}
+
 function getSessionID() //This is a placeholder function, to be replaced when the server is entirely running.
 {
   /*
@@ -180,9 +178,11 @@ function jsonParser(miscommunication) {
 
     testOb=miscommunication.boxes[j];
    //Ignore; bug fixing console.log(testOb)
-    tempID=miscommunication.boxes[j].tileId;
-    testOb.tileId=j+1;
-    goodboard.boxes[tempID.toString()]=testOb;
+   // tempID=miscommunication.boxes[j].tileId;
+   totalB+=1
+   asdf=testOb.tileId
+   testOb.tileId=j+1;
+    goodboard.boxes[asdf.toString()]=testOb;
   }
   drawLocked();
   // FOR NOW console.log("MRS:")
@@ -202,11 +202,69 @@ eventuallyRemove=[];
 }
 //Primary Functions
 
-function newTile(Box) {
+
+function silentNew(id, Box) {
   //console.log(Box);
-  var box=new Tile(totalB+1,calcX(Box.tileId%14),calcY(Box.tileId),Box.tileNum,Box.owner,Box.enabled);
-  totalB+=1;
-  currentArray.push(box);  
+//  document.getElementById("tile" + id).remove();
+  var box=new Tile(id,calcX(Box.tileId%14),calcY(Box.tileId),Box.tileNum,Box.owner,Box.enabled);
+    var tile_div=document.getElementById("tile" + id);
+    tile_div.className='tile';
+    tile_div.innerHTML=(box.value).toString()+"<div><div style=\"font-size:1vmin;transform: translate(0, -3.2vmin);\">"+players[Box.owner-1]+"</div></div>"
+    tile_div.id='tile'+(box.id).toString();
+    //grid.appendChild(tile_div);
+
+   // document.getElementById('tile'+(Tile.id).toString()).style.transform="translate(1vmin,0vmin)" //Static transform to accomodate for the earlier margin one
+    document.getElementById('tile'+(box.id).toString()).style.transform="translate("+(transformnumx*(box.x-1))+"vmin,"+((box.y-1)*transformnumy)+"vmin)" //Original position transform
+    document.getElementById('tile'+(box.id).toString()).style.backgroundColor='#'+getColor(box.value);
+    document.getElementById('tile'+(box.id).toString()).style.transform=document.getElementById('tile'+(box.id).toString()).style.transform+" translateX(0vmin)"+" translateY(0vmin)"; //Original position transform
+    document.getElementById('tile'+(box.id).toString()).style.color='#'+darkOrLight(getColor(box.value));
+
+    switch(Box.tileNum) {
+      case(2):
+      case(4):
+      case(8):
+          document.getElementById('tile'+(box.id).toString()).style.fontSize='2.5vmin'; //5 max
+          document.getElementById('tile'+(box.id).toString()).style.lineHeight='5vmin';
+          break;
+      case(16):
+      case(32):
+      case(64):
+        document.getElementById('tile'+(box.id).toString()).style.fontSize='2.5vmin'; //4.6 Max
+        document.getElementById('tile'+(box.id).toString()).style.lineHeight='5vmin';
+        break;
+      case(128):
+      case(256):
+      case(512):
+    //    document.getElementById('tile'+(box.id).toString()).style.marginLeft='255vmin'
+        document.getElementById('tile'+(box.id).toString()).style.fontSize='2vmin'; // 2.8 max
+        document.getElementById('tile'+(box.id).toString()).style.lineHeight='5vmin';
+        break;  
+      case(1024):
+      case(2048):
+      case(4096):
+      case(8192):
+         document.getElementById('tile'+(box.id).toString()).style.fontSize='2vmin'; // 2 max
+         document.getElementById('tile'+(box.id).toString()).style.lineHeight='5vmin';
+         break;    
+     case(16384):
+          document.getElementById('tile'+(box.id).toString()).style.fontSize='1.7vmin'; //1.5 optimal
+          document.getElementById('tile'+(box.id).toString()).style.lineHeight='5vmin';
+          break;
+    }
+    
+
+  }
+
+
+
+
+
+
+
+
+function newTile(id, Box) {
+  //console.log(Box);
+  var box=new Tile(id,calcX(Box.tileId%14),calcY(Box.tileId),Box.tileNum,Box.owner,Box.enabled);
     var tile_div=document.createElement('div');
     tile_div.className='tile';
     tile_div.innerHTML=(box.value).toString()+"<div><div style=\"font-size:1vmin;transform: translate(0, -3.2vmin);\">"+players[Box.owner-1]+"</div></div>"
@@ -277,9 +335,8 @@ function newTile(Box) {
 
   }
 function drawLocked() {
-  if (!gamestarted==false) {
-    $('.blocked').remove()
-  }
+  $('.blocked').remove()
+  
   for (let i=0;i<lockedBoxes.length;i++) {
     
     var blocked_tile=document.createElement('div');
@@ -294,33 +351,27 @@ function drawLocked() {
   }
   $(".blocked").css("background-color","#"+JSON.parse($.cookie("boardTheme"))["blocked"])  
 }
-function idInCurrentArray(id) {
-  for (let i=0;i<currentArray.length;i++) {
-    if (currentArray[i].id==parseInt(id,10)) {
-      return [true,i];
-      break;
-    }
-  }
-  return false;
-}
 
-function moveTile(Tile,FutureTile) { //TIle is the tile as it sits NOW, FutureTile is where you want it to move.
-//console.log(Tile.y)
+
+function moveTile(id,Tile,FutureTile) { //TIle is the tile as it sits NOW, FutureTile is where you want it to move.
+  okayWork=false;
   var progress=0;
+    //console.log("THE ID IS "+ id + Tile.tileId + FutureTile.tileId)
+    console.log("Finding the current animation of " + id + ". It is: " + findCurrentAnim(id,"X"));
     anime({
-      targets: '#'+'tile'+(Tile.id).toString(),
+      targets: '#'+'tile'+id,
       translateY:{
         
-        value:['0vmin',((FutureTile.y-Tile.y)*transformnumy).toString()+'vmin'],
-        duration:300,
+        value:[findCurrentAnim(id,"Y"),(((calcY(FutureTile.tileId))-calcY(Tile.tileId))*transformnumy).toString()+'vmin'],
+        duration:1000,
     },
       translateX:{
-        value:['0vmin',((FutureTile.x-Tile.x)*transformnumx).toString()+'vmin'],        //value:[5.735*0,5.735*-13],
-        duration:300,
+        value:[findCurrentAnim(id,"X"),(((((calcX(FutureTile.tileId)%14))-(calcX(Tile.tileId)%14)))*transformnumx).toString()+'vmin'],        //value:[5.735*0,5.735*-13],
+        duration:1000,
       },
   
       backgroundColor: [{
-        value:['#'+getColor(Tile.value),'#'+getColor(FutureTile.value)],
+        value:['#'+getColor(Tile.tileNum),'#'+getColor(FutureTile.tileNum)],
         duration:300,
       }
     ],
@@ -328,22 +379,24 @@ function moveTile(Tile,FutureTile) { //TIle is the tile as it sits NOW, FutureTi
       easing: 'easeInOutQuad',
       update: function() {
         progress+=1
-        if (progress>40) {
-          document.getElementById('tile'+(Tile.id).toString()).style.color='#'+darkOrLight(getColor(FutureTile.value));
-        document.getElementById('tile'+(Tile.id).toString()).innerHTML=(FutureTile.value).toString()+"<div><div style=\"font-size:1vmin;transform: translate(0, -3.2vmin);\">"+players[FutureTile.owner-1]+"</div></div>"
+        if (progress>40 && progress<50) {
+          document.getElementById('tile'+(id).toString()).style.color='#'+darkOrLight(getColor(FutureTile.tileNum));
+        document.getElementById('tile'+(id).toString()).innerHTML=(FutureTile.tileNum).toString()+"<div><div style=\"font-size:1vmin;transform: translate(0, -3.2vmin);\">"+players[FutureTile.owner-1]+"</div></div>"
+        }
+        if (progress>99) {
+          silentNew(id,FutureTile);
+          okayWork=true;
         }
       }
     
     
     })
     //document.getElementById('tile'+(Tile.id).toString()).innerHTML=(FutureTile.value).toString()+'\n'; This is a blanket update, the progress updater above will do a better job 99% of the time, but uncomment this if a corner-case arises
-    currentArray[idInCurrentArray(Tile.id)[1]]=FutureTile;
-    console.log(currentArray);
   }
-function deleteTile(Tile) { //Play delete animation then kick that sorry thing off of the array.
+function deleteTile(id, Tile) { //Play delete animation then kick that sorry thing off of the array.
   console.log("DELETING " +id)
   anime({
-    targets: '#'+'tile'+(Tile.id).toString(),
+    targets: '#'+'tile'+id,
     scale:[{
       value:0,
       duration:300,
@@ -355,7 +408,7 @@ function deleteTile(Tile) { //Play delete animation then kick that sorry thing o
   },
 ],
     backgroundColor: [{
-      value:['#'+getColor(Tile.value), '#FFFFFF'],
+      value:['#'+getColor(Tile.tileNum), '#FFFFFF'],
       duration:300,
     },
   ],
@@ -364,94 +417,76 @@ function deleteTile(Tile) { //Play delete animation then kick that sorry thing o
     
   
   })
-  currentArray.splice(findposbyID(Tile.id),1);//Remove that filthy, filthy div from existence as we know it.
-  eventuallyRemove.push((Tile.id).toString());
-}
-function firstDraw(tempBoard) { //When the board is first recieved, call this function on it.
-  
-  for (let i=1; i<(Object.keys(tempBoard.boxes).length+1);i++) {
-     //console.log(Board.boxes[i.toString()]);
-//     console.log(tempBoard.boxes[i+1]) 
-     newTile(tempBoard.boxes[i]);
-    }
-    console.log("Setup Complete. Now Waiting.");
-
+  eventuallyRemove.push((id).toString());
+  document.getElementById('tile'+id).remove();
 }
 
 function drawMovement(newBoard) {
-
 board=newBoard;
+oldnew= JSON.parse(JSON.stringify(newBoard));
   //Find Key Differences
 newArrayKeys=(Object.keys(newBoard.boxes));
 newArrayKeys=newArrayKeys.map(function (x) { 
   return parseInt(x, 10); 
 });
+console.log("THE NEW BOARD IS: "+ newArrayKeys)
 //console.log(newArrayKeys)
-currentArrayKeys=[];
-for (i=0;i<currentArray.length;i++) {
-  currentArrayKeys.push(currentArray[i].id);
-}
+currentArrayKeys=Object.keys(oldBoard.boxes);
+currentArrayKeys=currentArrayKeys.map(function (x) { 
+  return parseInt(x, 10); 
+});
+console.log("THE OLD BOARD IS: "+ currentArrayKeys)
 //First, you find if there are any elements newArrayKeys has that the current one doesn't (additions)
+
 additions=[]
-additions2=[];
-for(i=0;i<newArrayKeys.length;i++) {
-  found=false;
-  for(j=0;j<currentArrayKeys.length;j++) {
-    if (newArrayKeys[i]==currentArrayKeys[j]) {
-      found=true;
-    }
-  }
-  
-  if (!found) {
-    additions.push(newArrayKeys[i]);
-    additions2.push(newArrayKeys[i]);
-  }
-}
+additions=newArrayKeys.filter(x => !currentArrayKeys.includes(x))
+console.log("Done adding " + additions)
 //console.log(additions);
 //Then, you check to see if there are any elements (by id) that the old array has and new doesn't  (deletions)
 deletions=[]
-for(i=0;i<currentArrayKeys.length;i++) {
-  found=false;
-  for(j=0;j<newArrayKeys.length;j++) {
-    if (currentArrayKeys[i]==newArrayKeys[j]) {
-      found=true;
-    }
-  }
-  
-  if (!found) {
-    deletions.push(currentArrayKeys[i]);
-  }
-}
+console.log("The board will contain "+ newArrayKeys)
+console.log("The board currently contains"+ currentArrayKeys)
+
+deletions=currentArrayKeys.filter(x => !newArrayKeys.includes(x))
+
+console.log("Done deleting " + deletions)
+
 //console.log(deletions);
 //Remove these from the lists; they'll be parsed seperately
 //Parse Deletions
 do{
   if (deletions.length>0) {
-    deleteTile(currentArray[idInCurrentArray(deletions[0])[1]]);
+    deleteTile(deletions[0],oldBoard.boxes[deletions[0]]);
     deletions.shift();
   }
 }while(deletions.length>0); 
 do{
   if (additions.length>0) {
-  newTile(newBoard.boxes[additions[0]]);
+  newTile(additions[0],newBoard.boxes[additions[0]]);
   delete newBoard.boxes[additions[0]];
+  delete currentArrayKeys[additions[0]];
   additions.shift();
   }
   }while(additions.length>0); 
 
-ketamine=Object.keys(newBoard.boxes);
+
+ketamine=currentArrayKeys.filter(x => newArrayKeys.includes(x));;
+console.log(ketamine);
 var i=0;
   for (i in ketamine) {
     if (true /*idInCurrentArray(ketamine)[0]*/) {
       if (debug) {
       console.log("RECIEVED"+i)
       }
-      Box=newBoard.boxes[ketamine[i]];
-
-      moveTile(currentArray[idInCurrentArray(ketamine[i])[1]],new Tile(ketamine[i],calcX(Box.tileId%14),calcY(Box.tileId),Box.tileNum,Box.owner,Box.enabled));
+      console.log(ketamine[i] + 'SENT')
+      moveTile(ketamine[i],oldBoard.boxes[ketamine[i]],newBoard.boxes[ketamine[i]]);
     //  console.log(Box)
     }
    }
+
+  console.log("THE OLD BOARD WAS" + Object.keys(oldBoard.boxes))
+  oldBoard=oldnew;
+  console.log("THE OLD BOARD IS" + Object.keys(oldBoard.boxes))
 
 
 }
@@ -461,7 +496,7 @@ var i=0;
 document.addEventListener('keyup', function(event){
   var socket = new WebSocket('ws://127.0.0.1:8000'); 
   //alert(event.keyCode); (Uncomment this line if you need to add future keyswitch codes)
-  if (true && gamestarted) {
+  if (true && gamestarted && okayWork) {
     switch(event.keyCode) {
       case 87:
       case 38:
@@ -606,20 +641,13 @@ lightColor=JSON.parse($.cookie("boardTheme"))["lightColor"];
     console.log(data.msgType)
     switch(data.msgType) {
       case 'boardUpdate':
-        console.log(currentArray)
         testBoard=jsonParser(data.board)
         $.cookie("lastBoard", JSON.stringify(testBoard)); //Also log currentArray, somehow, in order to redraw smooothly.
         //console.log(testBoard);
-        console.log(testBoard)
-
-        if (gamestarted==false) {
-        firstDraw(testBoard);
+        drawMovement(testBoard);
         gamestarted=true;
-        }
-        else {
-          drawMovement(testBoard);
-        } 
-      break
+        recentboard=jsonParser(data.board);
+        break
       case 'waitingForPlayers':
         if (myPlayerNum==4) {
           myPlayerNum=4-data.numLeft;
